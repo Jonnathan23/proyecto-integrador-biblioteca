be1aa797cbbcab1dfecc10d58868336e0d9f04be
Erika Villa <114946749+Erika05cristina@users.noreply.github.com>
2024-05-23 23:13:19 -0500

    footer, bienvenida

--- biblioteca-saquicela-villa/src/app/app.component.ts
+++ biblioteca-saquicela-villa/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BienvenidaComponent } from './pages/bienvenida/bienvenida.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BienvenidaComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
