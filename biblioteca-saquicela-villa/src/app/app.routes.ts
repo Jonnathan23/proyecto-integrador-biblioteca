04971fb819779ff0ea305593f52847aebac0519c
Jonnathan23 <jsaquicela26@gmail.com>
2024-05-24 23:15:44 -0500

    Redireccionamiento a la pagina de bienvenida

--- biblioteca-saquicela-villa/src/app/app.routes.ts
+++ biblioteca-saquicela-villa/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ModulebooksComponent } from './modulebooks/modulebooks.component';
-import { ModulesessionComponent } from './modulesession/modulesession.component';
+import { BienvenidaComponent } from './pages/bienvenida/bienvenida.component';
+
 
 export const routes: Routes = [
     {path:'modulebooks', component:ModulebooksComponent},
-    {path:'sesion', component:ModulesessionComponent}
+    {path:'bienvenido', component:BienvenidaComponent},
+    {path: '', redirectTo:'bienvenido', pathMatch:'full'}
 ];
