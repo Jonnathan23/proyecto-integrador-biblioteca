4ac726d151a8d759201039ac94e44a38a4ea2ec0
Jonnathan23 <jsaquicela26@gmail.com>
2024-05-27 17:34:05 -0500

    Push de guardado

--- biblioteca-saquicela-villa/src/app/services/datauser.service.ts
+++ biblioteca-saquicela-villa/src/app/services/datauser.service.ts
@@ -42,8 +42,10 @@ export class DatauserService {
 
   async back(){
     try{
+      signOut(getAuth(this.firebaseApp))
       this.router.navigate(['/bienvenido'])
     }catch(e){
+      console.log(`Error al iniciar sesion\n${e}`)
 
     }
   }
